15f396e8b2708396359ab9972e355e466ef079bf
Matthew Feickert <matthew.feickert@cern.ch>
2022-06-20 01:05:21 -0500

    Update CPython

--- build_pyenv_python.sh
+++ build_pyenv_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-_CPYTHON_VERSION=3.10.3
+_CPYTHON_VERSION=3.10.4
 # Use --enable-shared to allow for ROOT to build
 echo "# Building CPython v${_CPYTHON_VERSION}"
 PYTHON_CONFIGURE_OPTS="--with-ensurepip --enable-optimizations --with-lto --enable-loadable-sqlite-extensions --enable-ipv6 --enable-shared" \
